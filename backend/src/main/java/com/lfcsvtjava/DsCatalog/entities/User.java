3e4edf3c538e8e00d80df9fda5909a359d03fc26
lfcsvt <lfcsvt@gmail.com>
2020-10-27 09:14:51 +0000

    authorization server implemented

--- backend/src/main/java/com/lfcsvtjava/DsCatalog/entities/User.java
+++ backend/src/main/java/com/lfcsvtjava/DsCatalog/entities/User.java
@@ -1,8 +1,10 @@
 package com.lfcsvtjava.DsCatalog.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,11 +16,14 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements UserDetails, Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -125,6 +130,51 @@ public class User implements Serializable{
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getAuthority()))
+				.collect(Collectors.toList());
+	}
+
+
+
+	@Override
+	public String getUsername() {
+		
+		return email;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+
+
+	@Override
+	public boolean isEnabled() {
 		return true;
 	}
 
