00da4d00f987eb9b56ab53b0f3d9c9594657e82f
lfcsvt <lfcsvt@gmail.com>
2020-10-27 10:38:17 +0000

    h2 security config

--- backend/src/main/java/com/lfcsvtjava/DsCatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/lfcsvtjava/DsCatalog/config/ResourceServerConfig.java
@@ -1,7 +1,11 @@
 package com.lfcsvtjava.DsCatalog.config;
 
+import java.util.Arrays;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,6 +16,8 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+	@Autowired
+	private Environment environment;
 	
 	@Autowired
 	private JwtTokenStore tokenStore;
@@ -29,6 +35,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+		if (Arrays.asList(environment.getActiveProfiles()).contains("test")){
+			http.headers().frameOptions().disable();
+		}
+		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
 		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
